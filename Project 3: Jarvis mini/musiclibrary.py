3b35ea2737278e4e3dfb3b9602e61cf526b2860e
Alpesh Solanki <40392553+Alpesh009@users.noreply.github.com>
2025-12-10 12:21:34 +0530

    Add files via upload

---
+++ Project 3: Jarvis mini/musiclibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "gitar": "https://www.youtube.com/watch?v=4jKkT3d8gdI&list=RD4jKkT3d8gdI&start_radio=1",
+    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo&list=RDsZrTJesvJeo&start_radio=1",
+    "kalu": "https://www.youtube.com/watch?v=3w7hhqBNoDY&list=RD3w7hhqBNoDY&start_radio=1"
+}
